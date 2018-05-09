427d822bae1e0a41e730ca5bbc8dfa373b314920
Steve HOLWEG <skytep@gmail.com>
2018-05-09 17:45:18 +0200

    Add project files.

---
+++ Docker.AutoDl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Docker.AutoDl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
